cf2440e13129760179461814ac1d66cef824f0c8
triple-s-rank <specialforwar@icloud.com>
2022-07-15 16:21:05 +0300

    Calculate salaries and vacancies for languages

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from statistics import mean
+
 import requests
 
 
@@ -6,16 +8,35 @@ def predict_rub_salary(vacancy):
     if not salary or not salary['currency'] == 'RUR':
         return None
     if salary['from'] and salary['to']:
-        return salary['from'] + salary['to'] / 2
+        return int(salary['from'] + salary['to'] / 2)
     elif salary['from']:
-        return salary['from'] * 1.2
+        return int(salary['from'] * 1.2)
     else:
-        return salary['to'] * 0.8
+        return int(salary['to'] * 0.8)
 
 
 params = {'area': 1, 'text': 'Python разработчик'}
-response = requests.get(url='https://api.hh.ru/vacancies', params=params)
+url = 'https://api.hh.ru/vacancies'
 
 
+def main():
+    vacancies_dict = {}
+    for language in ('Python', 'Java', 'Javascript'):
+        response = requests.get(url=url, params={'area': 1, 'text': f'{language} разработчик'})
+        all_salaries = []
+        vacancies_processed = 0
+        for vacancy in response.json()['items']:
+            all_salaries.append(predict_rub_salary(vacancy))
+            vacancies_processed += 1
+        all_salaries = [salary for salary in all_salaries if salary]
+
+        vacancies_dict[f'{language}'] = {
+            'vacancies_found': response.json()['found'],
+            'vacancies_processed': vacancies_processed,
+            'average_salary': int(mean(all_salaries))
+        }
+    return vacancies_dict
 
 
+if __name__ == '__main__':
+    print(main())
